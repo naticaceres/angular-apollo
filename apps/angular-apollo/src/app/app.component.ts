428d391ada56bf5b028cf96e8d4cdc2de9aa4712
Nati <natalia@savvyy.ai>
2022-02-16 14:33:07 -0500

    initial commit

---
+++ apps/angular-apollo/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'angular-apollo-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'angular-apollo';
+}
